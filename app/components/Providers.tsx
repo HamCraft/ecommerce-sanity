1610f709662f421e68b0491341ae377e022058b3
Ahmed Yaqoob Dhedhi <dhedhiahmedyaqoob@gmail.com>
2024-12-30 23:16:40 +0500

    99.9%

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,7 +9,7 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
+      successUrl="https://ecommerce-sanity-cyan.vercel.app/stripe/success"
       cancelUrl="https://ecommerce-sanity-cyan.vercel.app/stripe/error"
       currency="USD"
       billingAddressCollection={true}
